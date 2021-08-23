7c5d975f4f8add76edf3b7a31ac001addeb912a7
HyoJuns <gywns1720@naver.com>
2021-08-23 14:19:36 +0900

    gg

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,6 @@
+// Nav
+const $navBtn = document.querySelector('.nav__button');
+const $navList = document.querySelector('.nav__gnb--mobile');
 
 // Image Dom
 const $elemImage = document.querySelector('.article__part1');
@@ -14,11 +17,16 @@ const $elemsBtn = document.querySelectorAll('.button__container button');
 
 window.onload = function(){
     // Image Name
-    const device = ['desktop-image-hero-', 'mobile-image-hero-']
+    const devices = ['desktop', 'mobile']
     const parseData = JSON.parse(JSON.stringify(Params));
 
+    const $hamburger = document.getElementById('hamburger');
+    const $close = document.getElementById('close');
+
+    let NAV_BUTTON_CLICK_LOGIC = false;
     console.log(parseData);
     let page = 0;
+    let timer;
     const TOTAL_PAGE = 3;
     // 링크 방지
     $hyperlink.forEach(item => {
@@ -27,7 +35,8 @@ window.onload = function(){
         })
     });
 
-    // 버튼
+
+    // Next, Prev Button
     $elemsBtn.forEach(item => {
        item.addEventListener('click', function(e)
        {
@@ -50,10 +59,53 @@ window.onload = function(){
        });
     });
 
+    // Nav Check
+    $navBtn.addEventListener('click',function(){
+        navCheck();
+    });
+
     function pageCheck()
     {
+        let device;
         $title.innerHTML = `${parseData.data[page].title}`
         $description.innerHTML = `${parseData.data[page].desc}`
-        $elemImage.style.backgroundImage = `url(src/images/desktop${parseData.data[page].image})`;
+        if(window.innerWidth <= 1000)
+        {
+            device = devices[1];
+        }
+        else
+        {
+            device = devices[0];
+        }
+        $elemImage.style.backgroundImage = `url(src/images/${device}${parseData.data[page].image})`;
     }
+
+    function navCheck()
+    {
+        console.log($hamburger);
+        if(NAV_BUTTON_CLICK_LOGIC)
+        {
+            $hamburger.style.display = "none";
+            $close.style.display = "block";
+            $navList.classList.remove('nav__gnb--none');
+        }
+        else
+        {
+            $close.style.display = "none";
+            $hamburger.style.display = "block";
+            $navList.classList.add('nav__gnb--none');
+        }
+
+        NAV_BUTTON_CLICK_LOGIC = !NAV_BUTTON_CLICK_LOGIC;
+    }
+    navCheck();
+    window.addEventListener('resize', function(e){
+        if (timer)
+        {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function(){
+            pageCheck();
+        }, 500);
+    });
 }
